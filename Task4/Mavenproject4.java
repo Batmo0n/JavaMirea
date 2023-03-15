b3a5ae990c6cc097c7e5640ef06f09cdf43023f4
Batmo0n <bersenev.yaroslav.03@mail.ru>
2023-03-15 19:27:39 +0300

    new file:   Task4/Mavenproject4.java 	new file:   Task4/Telephone.java

---
+++ Task4/Mavenproject4.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mireatest.mavenproject4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ya
+ */
+public class Mavenproject4 {
+
+    public static void main(String[] args) {
+        System.out.println("Вариант 2, РИБО-03-21, Берсенев Ярослав");
+        Scanner myObj = new Scanner(System.in);
+        ArrayList<Telephone> list = new ArrayList<>();
+        list.add(new Telephone("Apple", "red", "123qwe", true));
+        list.add(new Telephone("Samsung", "white", "456rty", false));
+        list.add(new Telephone("Panasonic", "Black", "789uio", true));
+        while (true){
+            System.out.println("=================================");
+            for (Telephone phone: list){
+                    System.out.println(phone.toString());
+                }
+            System.out.println("=================================");
+            System.out.println("action: new, delete, clear, exit");
+            String action = myObj.nextLine();
+            System.out.println("------------------------------------");
+            switch (action) {
+                case "new":
+                    {
+                        System.out.println("What model? ");
+                        String model = myObj.nextLine();
+                        System.out.println("What color? ");
+                        String color = myObj.nextLine();
+                        System.out.println("What serial? ");
+                        String serial = myObj.nextLine();
+                        boolean isExist = false;
+                        for (Telephone phone: list){
+                            if(serial.equals(phone.getSerial())){
+                                isExist = true;
+                            }
+                        }
+                        System.out.println("Mobile: yes or no? ");
+                        String mobileAsk = myObj.nextLine();
+                        boolean mobile = false;
+                        if (mobileAsk.equals("yes")){
+                            mobile = true;
+                        }
+                        if (isExist==true){
+                            System.out.println("Такой серийник уже есть");
+                            break;
+                        }
+                        else {
+                            list.add(new Telephone(model,color,serial,mobile));
+                        }
+                        break;
+                    }
+                case "delete":
+                    {
+                        System.out.println("What serial? ");
+                        String serial = myObj.nextLine();
+                        for (Telephone phone: list){
+                            if (serial.equals(phone.getSerial())){
+                                list.remove(phone);
+                            }
+                        }       break;
+                    }
+                case "clear":
+                    list.clear();
+                    break;
+                case "exit":
+                    System.out.println("Выхода нет ха-ха");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Task4/Telephone.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mireatest.mavenproject4;
+
+/**
+ *
+ * @author Ya
+ */
+public class Telephone {
+    private String model;
+    private String color;
+    private String serial;
+    private boolean mobile;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public Telephone(String model, String color, String serial, boolean mobile) {
+        this.model = model;
+        this.color = color;
+        this.serial = serial;
+        this.mobile = mobile; 
+    }
+
+    @Override
+    public String toString() {
+        return model + " " + color + " " + serial + " " + mobile;
+    }
+    
+
+    
+}
+
